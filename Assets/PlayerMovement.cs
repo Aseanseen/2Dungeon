353f9b618ad587ea9cdd71c9c09eb3194878f1d7
Aseanseen <sean.lim.110@gmail.com>
2020-05-18 22:31:02 +0800

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 15f;
+    public Rigidbody2D rb;
+    public Animator anim;
+    Vector2 movement;
+
+    // Update is called once per frame
+    // Better to handle input here
+    void Update()
+    {
+    	movement.x = Input.GetAxisRaw("Horizontal");
+    	movement.y = Input.GetAxisRaw("Vertical");
+
+    	anim.SetFloat("Horizontal", movement.x);
+    	anim.SetFloat("Vertical", movement.y);
+    	anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    // Better not to put physics stuff into update, put into fixedupdate
+    // Better to handle movement
+    void FixedUpdate()
+    {
+    	move();
+    }
+    void move(){
+    	rb.velocity = new Vector2(movement.x,movement.y) * moveSpeed;
+//    	rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}

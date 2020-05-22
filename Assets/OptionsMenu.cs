dbcf97faec7a3bc0478a5072a8368895d43dc05f
Aseanseen <sean.lim.110@gmail.com>
2020-05-22 15:50:35 +0800

    Added pause menu

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+	public TMPro.TMP_Dropdown qualityDropdown;
+	public AudioMixer audioMixer;
+
+	// Default to the "High" option
+	public void Start(){
+		qualityDropdown.value = 2;
+	}
+    public void SetVolume(float volume){
+    	audioMixer.SetFloat("MasterVolume", volume);
+//    	audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+    public void SetQuality (int qualityIndex){
+    	qualityIndex = qualityDropdown.value;
+    	QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void Update()
+    {
+        Debug.Log(QualitySettings.GetQualityLevel());
+    }
+}
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	// static because don't want to reference this var
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+        	if(isPaused){
+        		Resume();
+        	}
+        	else{
+        		Pause();
+        	}
+		}
+    }
+    public void Resume(){
+    	// Sets the menu to be inactive
+    	pauseMenuUI.SetActive(false);
+    	// Set time
+    	Time.timeScale = 1f;
+    	isPaused = false;
+    }
+    void Pause(){
+    	// Sets the menu to be active
+    	pauseMenuUI.SetActive(true);
+    	// Freeze time
+    	Time.timeScale = 0f;
+    	isPaused = true;
+    }
+    public void LoadMenu(){
+    	Time.timeScale = 1f;
+    	SceneManager.LoadScene("StartMenu");
+    }
+	public void QuitGame(){
+    	Application.Quit();
+    }
+}

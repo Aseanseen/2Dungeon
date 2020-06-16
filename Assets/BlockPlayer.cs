9c6e5bec4833da2ef1372ac9ef5d157012206ba6
Aseanseen <sean.lim.110@gmail.com>
2020-06-16 13:09:06 +0800

    added some intro speech

---
+++ Assets/BlockPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlayer : MonoBehaviour
+{
+	public GameObject lightCheck;
+	Collider2D selfCollider;
+
+	void Start()
+    {
+        selfCollider = GetComponent<Collider2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightCheck == null){
+        	selfCollider.enabled = true;
+        }
+        else{
+        	selfCollider.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PromptController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptController : MonoBehaviour
+{
+	public PlayerMovement playerMovement;
+	public PlayerCombat playerCombat;
+	public Button attackButton;
+
+	bool wait = false;
+
+//	public List<GameObject> speechList;
+	public GameObject speech;
+
+	// public GameObject words1;
+	// public GameObject words2;
+	// public GameObject words3;
+	// public GameObject words4;
+
+	// Creates a queue for speech
+//	Queue<GameObject> speechPool = new Queue<GameObject>();
+	public List<GameObject> speechList = new List<GameObject>();
+	int i = 0;
+//    GameObject[] speechList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement.enabled = false;
+        playerCombat.enabled = false;
+        attackButton.interactable = false;
+
+    	speech = speechList[i];
+    	Debug.Log(speech);
+    	speech.SetActive(true);
+    }
+    void Update()
+    {
+
+    	Debug.Log(speechList.Count);
+    	if (Input.touchCount > 0 && speechList.Count != 0 && !wait)
+    	{
+    		StartCoroutine(NextWord(speech));
+    	}
+    	if (speechList.Count == 0 && !wait)
+    	{
+    		Destroy(gameObject);
+    	}
+    	if (Input.GetButtonDown("Jump")){
+    		StartCoroutine(NextWord(speech));
+    	}
+    }
+    IEnumerator NextWord(GameObject thing)
+    {
+		speech.SetActive(false);
+    	i ++;
+    	speech = speechList[i];
+		speech.SetActive(true);
+		wait = true;
+		yield return new WaitForSeconds(2);
+		wait = false;
+    }
+}
